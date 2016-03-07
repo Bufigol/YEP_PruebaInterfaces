52c4f221bfd984408375381f80689c584b2a477d
Bufigol <felipevasquez.tort@gmail.com>
2016-03-07 17:18:42 +0100

    agrgado action bar con letras a y b

--- app/src/main/java/com/bufigol/yep/Main.java
+++ app/src/main/java/com/bufigol/yep/Main.java
@@ -2,6 +2,7 @@ package com.bufigol.yep;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
